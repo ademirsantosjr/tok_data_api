fc14bd9ff4f9cb5aa89fbb53b23f7e652f92793d
Ademir Santos <ademir.m.junior@gmail.com>
2024-03-12 01:27:43 -0300

    remove codigo comentado

--- src/main/java/com/usuarios/cadastro/CadastroApplication.java
+++ src/main/java/com/usuarios/cadastro/CadastroApplication.java
@@ -35,61 +35,6 @@ public class CadastroApplication {
 		});
 	}
 
-//	@Bean
-//	public CommandLineRunner demo(UserRepository userRepository,
-//								  ProfileRepository profileRepository) {
-//		return (args -> {
-//
-//			var adminProfie = Profile.builder()
-//					.name("ADMIN")
-//					.build();
-//			var userProfile = Profile.builder()
-//					.name("USER")
-//					.build();
-//
-//			var persistedAdminProfile = profileRepository.save(adminProfie);
-//			var persistedUserProfile = profileRepository.save(userProfile);
-//
-//			var admin = User
-//					.builder()
-//					.name("admin")
-//					.email("admin@admin.com")
-//					.password(encodePass("4657"))
-//					.profile(persistedAdminProfile)
-//					.build();
-//
-//			var joao = User
-//					.builder()
-//					.name("João")
-//					.email("joao@toktok.com")
-//					.password("123456")
-//					.profile(persistedUserProfile)
-//					.build();
-//
-//			var maria = User
-//					.builder()
-//					.name("Maria")
-//					.email("maria@exemplo.com")
-//					.password("654321")
-//					.profile(persistedUserProfile)
-//					.build();
-//
-//			var pedro = User
-//					.builder()
-//					.name("Pedro")
-//					.email("pedro@top.com")
-//					.password("789456")
-//					.profile(persistedUserProfile)
-//					.build();
-//
-//			userRepository.save(admin);
-//			userRepository.save(joao);
-//			userRepository.save(maria);
-//			userRepository.save(pedro);
-//
-//		});
-//	}
-
 	private static String encodePass(String rawPassword) {
 		Map<String, PasswordEncoder> encoders = new HashMap<>();
 		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder =
